9f00b479175cd7b64d02e3d9bac44b92983e6fea
weazah <minimala978@gmail.com>
2023-04-13 01:53:10 +0000

    feat: floor and ceiling colors

--- src/game_loop.c
+++ src/game_loop.c
@@ -6,13 +6,32 @@
 /*   By: ozahir <ozahir@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 16:39:03 by tel-mouh          #+#    #+#             */
-/*   Updated: 2023/04/12 07:35:50 by ozahir           ###   ########.fr       */
+/*   Updated: 2023/04/13 01:51:20 by ozahir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cube3d.h"
 
+void	pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+ 
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
 
+void colorize_floor_and_ceiling(t_game	*game, int x)
+{
+	int st;
+	int en;
+
+	st = -1;
+	en = game->draw_end - 1;
+	while (++st < game->draw_start)
+		pixel_put(&game->img, x, st, game->color[0]);
+	while (++en < screenHeight - 1)
+		pixel_put(&game->img, x, en, game->color[1]);
+}
 int	game_loop(t_vars *vars)
 {
 	t_game	*game;
@@ -28,6 +47,7 @@ int	game_loop(t_vars *vars)
 	{
 		calcul_game(game, player, x_screen);
 		draw_line_to_img(game, x_screen);
+		colorize_floor_and_ceiling(game, x_screen);
 		// print_game_infos(game, player, x_screen, 3);
 	}
 	draw_img_to_window(vars);
@@ -37,7 +57,7 @@ int	game_loop(t_vars *vars)
 
 int	render(t_vars	*vars)
 {
-	if (vars->press.hor != -1)
+		if (vars->press.hor != -1)
 		move_horizontal(vars);
 	if (vars->press.ver != -1)
 		move_vertical(vars);
