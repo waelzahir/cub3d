46207e1414352c895b28b895bd0b038e3d4495d5
weazah <minimala978@gmail.com>
2023-04-15 06:07:00 +0000

    fix: fix vector polarity

--- src/parser/parser_parse_player_data.c
+++ src/parser/parser_parse_player_data.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser_parse_player_data.c                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tel-mouh <tel-mouh@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: ozahir <ozahir@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/01 01:13:32 by ozahir            #+#    #+#             */
-/*   Updated: 2023/04/06 05:20:08 by tel-mouh         ###   ########.fr       */
+/*   Updated: 2023/04/15 06:05:45 by ozahir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,12 +50,12 @@ void	set_dir_vec(t_vector *vec, char pole)
 	}
 	if (pole == 'E')
 	{
-		vec->dx = -1;
+		vec->dx = 1;
 		vec->dy = 0;
 	}
 	if (pole == 'W')
 	{
-		vec->dx = 1;
+		vec->dx = -1;
 		vec->dy = 0;
 	}
 }
